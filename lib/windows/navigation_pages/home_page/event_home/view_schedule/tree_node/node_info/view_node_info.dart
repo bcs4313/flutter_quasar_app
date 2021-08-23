338d04c21f94ba439a9c6ab986cf0f4b47ed86f8
bcs4313 <bcs4313@g.rit.edu>
2021-08-23 16:53:56 -0400

    - Nodes now report the time ranges that they were given by the event host to clients.

--- lib/windows/navigation_pages/home_page/event_home/view_schedule/tree_node/node_info/view_node_info.dart
+++ lib/windows/navigation_pages/home_page/event_home/view_schedule/tree_node/node_info/view_node_info.dart
@@ -15,11 +15,50 @@ class ViewNodeInfo extends StatelessWidget
   final GlobalKey<ScaffoldState> S_KEY = new GlobalKey<ScaffoldState>();
   ViewTreeNodeStatic base;
 
+  /// Basic Widget to generate upon getting a time range for a node
+  Widget rangeWidget = Container();
+
+  /// This String is generated when the user wants
+  /// to signify a start/end time.
+  String timeRangeString = "";
+
   /// Simple constructor for this view
   ///@param base the node holding the data needed to display this view
   ViewNodeInfo(ViewTreeNodeStatic base)
   {
     this.base = base;
+
+    if(base.startTime != "choose time" || base.startDate != "choose date" || base.endTime != "choose time"
+     || base.endDate != "choose date")
+      {
+        if(base.startDate != "choose date" && base.endDate != "choose date")
+          {
+            if(base.startTime == "choose time" || base.endTime == "choose time")
+              {
+                timeRangeString = "from " + base.startDate + "\n to " + base.endDate;
+              }
+            else
+              {
+                timeRangeString = "from " + base.startDate + ", " + base.startTime + "\n to " +
+                    base.endDate + ", " + base.endTime;
+              }
+          }
+        if(base.startTime != "choose time" && base.startDate != "choose time")
+          {
+            if(base.startDate == "choose date" || base.endDate == "choose date")
+            {
+              timeRangeString = "from " + base.startTime + "\n to " + base.endTime;
+            }
+          }
+      }
+    if(timeRangeString != "")
+      {
+        rangeWidget = Text(
+          "Time Range:",
+          style: TextStyle(fontSize: SizeConfig.scaleHorizontal * 8, height: 1.3, fontFamily: 'Roboto', color: Col.pink),
+          textAlign: TextAlign.center,
+        );
+      }
   }
 
   @override
@@ -68,6 +107,18 @@ class ViewNodeInfo extends StatelessWidget
                     ),
                   ),
                   ),
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 5 * SizeConfig.scaleVertical, left: 8 * SizeConfig.scaleHorizontal, right: 8 * SizeConfig.scaleHorizontal),
+                child: rangeWidget,
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 2 * SizeConfig.scaleVertical, left: 8 * SizeConfig.scaleHorizontal, right: 8 * SizeConfig.scaleHorizontal),
+                child: Text(
+                  timeRangeString,
+                  style: TextStyle(fontSize: SizeConfig.scaleHorizontal * 6, height: 1.3, fontFamily: 'Roboto', color: Col.pink),
+                  textAlign: TextAlign.center,
+                ),
               ),
             ],
           ),
